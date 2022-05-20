28e907c28e569f24784e5e7668cfae2d55e2c655
EmNoTrym <63888869+EmNoTrym@users.noreply.github.com>
2022-05-20 10:39:51 +0700

    implement_input_number

--- Assets/Spinner/Pointer.cs
+++ Assets/Spinner/Pointer.cs
@@ -4,18 +4,59 @@ using UnityEngine;
 
 public class Pointer : MonoBehaviour
 {
-    private Spinner spin;
+    public string pointerAt;
+
+    private Spinner spinner;
+    [SerializeField]
+    private int desireNum = 9;
 
     private void Start()
     {
-        spin = GameObject.Find("Wheel").GetComponent<Spinner>();
+        spinner = GameObject.Find("Wheel").GetComponent<Spinner>();
+    }
+
+    private void Update()
+    {
+        if (spinner.isSpinning)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                desireNum = 1;
+            }else if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                desireNum = 2;
+            }else if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                desireNum = 3;
+            }else if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                desireNum = 4;
+            }else if (Input.GetKeyDown(KeyCode.Alpha5))
+            {
+                desireNum = 5;
+            }else if (Input.GetKeyDown(KeyCode.Alpha6))
+            {
+                desireNum = 6;
+            }else if (Input.GetKeyDown(KeyCode.Alpha7))
+            {
+                desireNum = 7;
+            }else if (Input.GetKeyDown(KeyCode.Alpha8))
+            {
+                desireNum = 8;
+            }else if (Input.GetKeyDown(KeyCode.Alpha9))
+            {
+                desireNum = 9;
+            }
+        }
+
+        if (spinner.isSpinning && pointerAt != desireNum.ToString() && spinner.genSpeed < 0.4f)
+        {
+            spinner.genSpeed = 0.4f;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        /*
-        if (spin.isSpinning)
-            Debug.Log(other.gameObject.name);
-        */
+        pointerAt = other.gameObject.name;
     }
 }
