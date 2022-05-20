f0f8bd4703b49fa7a41e6e787c08ae2f9f8ebe77
EmNoTrym <63888869+EmNoTrym@users.noreply.github.com>
2022-05-20 23:29:16 +0700

    add_game_leves

--- Assets/Spinner/Pointer.cs
+++ Assets/Spinner/Pointer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pointer : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class Pointer : MonoBehaviour
     private Spinner spinner;
     [SerializeField]
     private int desireNum = 9;
+    [SerializeField]
+    private GameObject[] sectors;
 
     private void Start()
     {
@@ -19,31 +22,31 @@ public class Pointer : MonoBehaviour
     {
         if (spinner.isSpinning)
         {
-            if (Input.GetKeyDown(KeyCode.Alpha1))
+            if (Input.GetKeyDown(KeyCode.Alpha1) && sectors[0].activeSelf)
             {
                 desireNum = 1;
-            }else if (Input.GetKeyDown(KeyCode.Alpha2))
+            }else if (Input.GetKeyDown(KeyCode.Alpha2) && sectors[1].activeSelf)
             {
                 desireNum = 2;
-            }else if (Input.GetKeyDown(KeyCode.Alpha3))
+            }else if (Input.GetKeyDown(KeyCode.Alpha3) && sectors[2].activeSelf)
             {
                 desireNum = 3;
-            }else if (Input.GetKeyDown(KeyCode.Alpha4))
+            }else if (Input.GetKeyDown(KeyCode.Alpha4) && sectors[3].activeSelf)
             {
                 desireNum = 4;
-            }else if (Input.GetKeyDown(KeyCode.Alpha5))
+            }else if (Input.GetKeyDown(KeyCode.Alpha5) && sectors[4].activeSelf)
             {
                 desireNum = 5;
-            }else if (Input.GetKeyDown(KeyCode.Alpha6))
+            }else if (Input.GetKeyDown(KeyCode.Alpha6) && sectors[5].activeSelf)
             {
                 desireNum = 6;
-            }else if (Input.GetKeyDown(KeyCode.Alpha7))
+            }else if (Input.GetKeyDown(KeyCode.Alpha7) && sectors[6].activeSelf)
             {
                 desireNum = 7;
-            }else if (Input.GetKeyDown(KeyCode.Alpha8))
+            }else if (Input.GetKeyDown(KeyCode.Alpha8) && sectors[7].activeSelf)
             {
                 desireNum = 8;
-            }else if (Input.GetKeyDown(KeyCode.Alpha9))
+            }else if (Input.GetKeyDown(KeyCode.Alpha9) && sectors[8].activeSelf)
             {
                 desireNum = 9;
             }
@@ -53,10 +56,40 @@ public class Pointer : MonoBehaviour
         {
             spinner.genSpeed = 0.4f;
         }
+
+        if (spinner.isTurned & !spinner.isSpinning)
+        {
+            disableSector();
+            Teleport();
+        }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         pointerAt = other.gameObject.name;
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        pointerAt = "0";
+    }
+
+    private void Teleport()
+    {
+        Debug.Log("Teleporting to Level " + pointerAt);
+        spinner.isTurned = false;
+        SceneManager.LoadScene(desireNum);
+    }
+
+    private void disableSector()
+    {
+        foreach (var sector in sectors)
+        {
+            if (sector.name == pointerAt)
+            {
+                sector.gameObject.SetActive(false);
+            }
+        }
+    }
+
 }
--- Assets/Spinner/Spinner.cs
+++ Assets/Spinner/Spinner.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Linq;
 public class Spinner : MonoBehaviour
 {
     public float genSpeed;
     public float subSpeed;
     public bool isSpinning = false;
+    public bool isTurned = false;
     public AudioSource source;
     public AudioClip clip;
+
+    
 
     // Update is called once per frame
     void Update()
@@ -16,6 +19,7 @@ public class Spinner : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && !isSpinning)
         {
             SpinWheel();
+            isTurned = true;
         }
 
         if (isSpinning)
@@ -34,11 +38,11 @@ public class Spinner : MonoBehaviour
 
     public void SpinWheel()
     {
-        genSpeed = Random.Range(2.000f, 5.000f);
+        genSpeed = Random.Range(4.000f, 6.000f);
         subSpeed = Random.Range(0.003f, 0.005f);
         source.PlayOneShot(clip);
 
         isSpinning = true;
-        Debug.Log(genSpeed);
     }
+
 }
